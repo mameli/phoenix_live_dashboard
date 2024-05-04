f9d6bb4b51dbcbe377b2ef3657cf940080045c2a
Filippo Mameli <mameli93@gmail.com>
2024-05-04 16:12:48 +0200

    conditional color on sensor values

--- lib/phoenix_live_sensors_web/live/live_sensors.ex
+++ lib/phoenix_live_sensors_web/live/live_sensors.ex
@@ -3,7 +3,7 @@ defmodule PhoenixLiveSensorsWeb.LiveSensors do
 
   def mount(_params, _session, socket) do
     PhoenixLiveSensorsWeb.Endpoint.subscribe("topic")
-    {:ok, assign(socket, cpu_status: 0, gpu_status: 0)}
+    {:ok, assign(socket, cpu_status: 0, gpu_status: 0, cpu_color: "text-green-500", gpu_color: "text-green-500")}
   end
 
   # Handle parameters passed to the LiveView
@@ -11,11 +11,17 @@ defmodule PhoenixLiveSensorsWeb.LiveSensors do
     IO.puts("Received message")
     sensor_map = Jason.decode!(message)
     IO.inspect(sensor_map)
+    {value, ""} = Float.parse(Map.get(sensor_map, "value"))
+    color = case value do
+      v when v > 200 and v < 500-> "text-yellow-500"
+      v when v > 500 -> "text-red-500"
+      _ -> "text-green-500"
+    end
     case Map.get(sensor_map, "name") do
       "CPU Power" ->
-        {:noreply, assign(socket, cpu_status: Map.get(sensor_map, "value"))}
+        {:noreply, assign(socket, cpu_status: value, cpu_color: color)}
       "GPU Power" ->
-        {:noreply, assign(socket, gpu_status: Map.get(sensor_map, "value"))}
+        {:noreply, assign(socket, gpu_status: value, gpu_color: color)}
       _ ->
         {:noreply, socket}
     end
